d10a400cef1c4c6068551f34f32f509016015fde
Joe Maples <joe@frap129.org>
2016-07-14 20:33:01 +0000

    cpufreq: chill: Go back to using Conservative's tunables
    This reverts commit 67b54a2a6dc6ebb3ddfc45544c8016ed910f6d3a.
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_chill.c
    	drivers/cpufreq/cpufreq_chill.h
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/cpufreq/cpufreq_chill.c
+++ drivers/cpufreq/cpufreq_chill.c
@@ -14,6 +14,7 @@
 
 #include <linux/slab.h>
 <<<<<<< HEAD
+<<<<<<< HEAD
 #include "cpufreq_governor.h"
 <<<<<<< HEAD
 #include <linux/display_state.h>
@@ -85,6 +86,9 @@ unsigned int boost_counter = 0;
 =======
 #include "cpufreq_chill.h"
 >>>>>>> 7d019fa8484d... cpufreq: chill: Major cleanup, move changes from governor.h to chill.h
+=======
+#include "cpufreq_governor.h"
+>>>>>>> 55a9e8a34183... cpufreq: chill: Go back to using Conservative's tunables
 #ifdef CONFIG_POWERSUSPEND
 #include <linux/powersuspend.h>
 #endif
@@ -94,10 +98,14 @@ unsigned int boost_counter = 0;
 /* Chill version macros */
 #define CHILL_VERSION_MAJOR			(1)
 <<<<<<< HEAD
+<<<<<<< HEAD
 #define CHILL_VERSION_MINOR			(2)
 =======
 #define CHILL_VERSION_MINOR			(4)
 >>>>>>> 2486219a5e33... cpufreq: chill: use GOV_CHILL macro
+=======
+#define CHILL_VERSION_MINOR			(5)
+>>>>>>> 55a9e8a34183... cpufreq: chill: Go back to using Conservative's tunables
 
 >>>>>>> 7d019fa8484d... cpufreq: chill: Major cleanup, move changes from governor.h to chill.h
 /* Chill governor macros */
@@ -116,11 +124,15 @@ static inline unsigned int get_freq_target(struct cs_dbs_tuners *cs_tuners,
 =======
 static unsigned int boost_counter = 0;
 
+<<<<<<< HEAD
 static inline unsigned int get_freq_target(struct chill_dbs_tuners *chill_tuners,
 >>>>>>> 7d019fa8484d... cpufreq: chill: Major cleanup, move changes from governor.h to chill.h
+=======
+static inline unsigned int get_freq_target(struct cs_dbs_tuners *cs_tuners,
+>>>>>>> 55a9e8a34183... cpufreq: chill: Go back to using Conservative's tunables
 					   struct cpufreq_policy *policy)
 {
-	unsigned int freq_target = (chill_tuners->freq_step * policy->max) / 100;
+	unsigned int freq_target = (cs_tuners->freq_step * policy->max) / 100;
 
 	/* max freq cannot be less than 100. But who knows... */
 	if (unlikely(freq_target == 0))
@@ -138,12 +150,12 @@ static inline unsigned int get_freq_target(struct chill_dbs_tuners *chill_tuners
  * Any frequency increase takes it to the maximum frequency. Frequency reduction
  * happens at minimum steps of 5% (default) of maximum frequency
  */
-static void chill_check_cpu(int cpu, unsigned int load)
+static void cs_check_cpu(int cpu, unsigned int load)
 {
 	struct cs_cpu_dbs_info_s *dbs_info = &per_cpu(cs_cpu_dbs_info, cpu);
 	struct cpufreq_policy *policy = dbs_info->cdbs.cur_policy;
 	struct dbs_data *dbs_data = policy->governor_data;
-	struct chill_dbs_tuners *chill_tuners = dbs_data->tuners;
+	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 
 <<<<<<< HEAD
 	/* Create display state boolean */
@@ -159,7 +171,7 @@ static void chill_check_cpu(int cpu, unsigned int load)
 	 * break out if we 'cannot' reduce the speed as the user might
 	 * want freq_step to be zero
 	 */
-	if (chill_tuners->freq_step == 0)
+	if (cs_tuners->freq_step == 0)
 		return;
 
 <<<<<<< HEAD
@@ -218,7 +230,7 @@ static void chill_check_cpu(int cpu, unsigned int load)
 	} else if (!display_on && load <= cs_tuners->down_threshold_suspended) {
 =======
 	/* Check for frequency increase */
-	if (load > chill_tuners->up_threshold) {
+	if (load > cs_tuners->up_threshold) {
 
 		/* if we are already at full speed then break out early */
 		if (dbs_info->requested_freq == policy->max)
@@ -234,11 +246,18 @@ static void chill_check_cpu(int cpu, unsigned int load)
 		dbs_info->requested_freq += get_freq_target(cs_tuners, policy);
 =======
 		/* Boost if count is reached, otherwise increase freq */
+<<<<<<< HEAD
 		if (chill_tuners->boost_enabled && boost_counter >= chill_tuners->boost_count)
 			dbs_info->requested_freq += get_freq_target(chill_tuners, policy->max);
 		else
 			dbs_info->requested_freq += get_freq_target(chill_tuners, policy);
 >>>>>>> 7d019fa8484d... cpufreq: chill: Major cleanup, move changes from governor.h to chill.h
+=======
+		if (cs_tuners->boost_enabled && boost_counter >= cs_tuners->boost_count)
+			dbs_info->requested_freq = policy->max;
+		else
+			dbs_info->requested_freq += get_freq_target(cs_tuners, policy);
+>>>>>>> 55a9e8a34183... cpufreq: chill: Go back to using Conservative's tunables
 
 		if (dbs_info->requested_freq > policy->max)
 			dbs_info->requested_freq = policy->max;
@@ -250,11 +269,15 @@ static void chill_check_cpu(int cpu, unsigned int load)
 
 	/* Check for frequency decrease */
 <<<<<<< HEAD
+<<<<<<< HEAD
 	if (load < cs_tuners->down_threshold) {
 >>>>>>> 2be0437dd8e1... cpufreq: Add Chill cpu gov
 =======
 	if (load < chill_tuners->down_threshold) {
 >>>>>>> 7d019fa8484d... cpufreq: chill: Major cleanup, move changes from governor.h to chill.h
+=======
+	if (load < cs_tuners->down_threshold) {
+>>>>>>> 55a9e8a34183... cpufreq: chill: Go back to using Conservative's tunables
 		unsigned int freq_target;
 		/*
 		 * if we cannot reduce the frequency anymore, break out early
@@ -262,7 +285,7 @@ static void chill_check_cpu(int cpu, unsigned int load)
 		if (policy->cur == policy->min)
 			return;
 
-		freq_target = get_freq_target(chill_tuners, policy);
+		freq_target = get_freq_target(cs_tuners, policy);
 		if (dbs_info->requested_freq > freq_target)
 			dbs_info->requested_freq -= freq_target;
 <<<<<<< HEAD
@@ -310,7 +333,7 @@ static void chill_check_cpu(int cpu, unsigned int load)
 >>>>>>> 2be0437dd8e1... cpufreq: Add Chill cpu gov
 }
 
-static void chill_dbs_timer(struct work_struct *work)
+static void cs_dbs_timer(struct work_struct *work)
 {
 	struct cs_cpu_dbs_info_s *dbs_info = container_of(work,
 			struct cs_cpu_dbs_info_s, cdbs.work.work);
@@ -318,9 +341,10 @@ static void chill_dbs_timer(struct work_struct *work)
 	struct cs_cpu_dbs_info_s *core_dbs_info = &per_cpu(cs_cpu_dbs_info,
 			cpu);
 	struct dbs_data *dbs_data = dbs_info->cdbs.cur_policy->governor_data;
-	struct chill_dbs_tuners *chill_tuners = dbs_data->tuners;
-	int delay = delay_for_sampling_rate(chill_tuners->sampling_rate);
+	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
+	int delay = delay_for_sampling_rate(cs_tuners->sampling_rate);
 	bool modify_all = true;
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 =======
@@ -329,10 +353,13 @@ static void chill_dbs_timer(struct work_struct *work)
 =======
 	unsigned int sampling_rate_suspended = chill_tuners->sampling_rate * chill_tuners->sleep_depth;
 >>>>>>> 7d019fa8484d... cpufreq: chill: Major cleanup, move changes from governor.h to chill.h
+=======
+	unsigned int sampling_rate_suspended = cs_tuners->sampling_rate * cs_tuners->sleep_depth;
+>>>>>>> 55a9e8a34183... cpufreq: chill: Go back to using Conservative's tunables
 
 	mutex_lock(&core_dbs_info->cdbs.timer_mutex);
 
-	if (!need_load_eval(&core_dbs_info->cdbs, chill_tuners->sampling_rate))
+	if (!need_load_eval(&core_dbs_info->cdbs, cs_tuners->sampling_rate))
 		modify_all = false;
 <<<<<<< HEAD
 	else
@@ -374,12 +401,12 @@ static int dbs_cpufreq_notifier(struct notifier_block *nb, unsigned long val,
 }
 
 /************************** sysfs interface ************************/
-static struct common_dbs_data chill_dbs_cdata;
+static struct common_dbs_data cs_dbs_cdata;
 
 static ssize_t store_sampling_rate(struct dbs_data *dbs_data, const char *buf,
 		size_t count)
 {
-	struct chill_dbs_tuners *chill_tuners = dbs_data->tuners;
+	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 	unsigned int input;
 	int ret;
 	ret = sscanf(buf, "%u", &input);
@@ -387,55 +414,56 @@ static ssize_t store_sampling_rate(struct dbs_data *dbs_data, const char *buf,
 	if (ret != 1)
 		return -EINVAL;
 
-	chill_tuners->sampling_rate = max(input, dbs_data->min_sampling_rate);
+	cs_tuners->sampling_rate = max(input, dbs_data->min_sampling_rate);
 	return count;
 }
 
 static ssize_t store_up_threshold(struct dbs_data *dbs_data, const char *buf,
 		size_t count)
 {
-	struct chill_dbs_tuners *chill_tuners = dbs_data->tuners;
+	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 	unsigned int input;
 	int ret;
 	ret = sscanf(buf, "%u", &input);
 
-	if (ret != 1 || input > 100 || input <= chill_tuners->down_threshold)
+	if (ret != 1 || input > 100 || input <= cs_tuners->down_threshold)
 		return -EINVAL;
 
-	chill_tuners->up_threshold = input;
+	cs_tuners->up_threshold = input;
 	return count;
 }
 
 static ssize_t store_down_threshold(struct dbs_data *dbs_data, const char *buf,
 		size_t count)
 {
-	struct chill_dbs_tuners *chill_tuners = dbs_data->tuners;
+	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 	unsigned int input;
 	int ret;
 	ret = sscanf(buf, "%u", &input);
 
 	/* cannot be lower than 11 otherwise freq will not fall */
 	if (ret != 1 || input < 11 || input > 100 ||
-			input >= chill_tuners->up_threshold)
+			input >= cs_tuners->up_threshold)
 		return -EINVAL;
 
-	chill_tuners->down_threshold = input;
+	cs_tuners->down_threshold = input;
 	return count;
 }
 
 static ssize_t store_down_threshold_suspended(struct dbs_data *dbs_data, const char *buf,
 		size_t count)
 {
-	struct chill_dbs_tuners *chill_tuners = dbs_data->tuners;
+	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 	unsigned int input;
 	int ret;
 	ret = sscanf(buf, "%u", &input);
 
 	/* cannot be lower than 11 otherwise freq will not fall */
 	if (ret != 1 || input < 11 || input > 100 ||
-			input >= chill_tuners->up_threshold)
+			input >= cs_tuners->up_threshold)
 		return -EINVAL;
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 	cs_tuners->down_threshold_suspended = input;
@@ -445,13 +473,16 @@ static ssize_t store_down_threshold_suspended(struct dbs_data *dbs_data, const c
 =======
 	chill_tuners->down_threshold = input;
 >>>>>>> 7d019fa8484d... cpufreq: chill: Major cleanup, move changes from governor.h to chill.h
+=======
+	cs_tuners->down_threshold = input;
+>>>>>>> 55a9e8a34183... cpufreq: chill: Go back to using Conservative's tunables
 	return count;
 }
 
 static ssize_t store_ignore_nice_load(struct dbs_data *dbs_data,
 		const char *buf, size_t count)
 {
-	struct chill_dbs_tuners *chill_tuners = dbs_data->tuners;
+	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 	unsigned int input, j;
 	int ret;
 
@@ -462,10 +493,10 @@ static ssize_t store_ignore_nice_load(struct dbs_data *dbs_data,
 	if (input > 1)
 		input = 1;
 
-	if (input == chill_tuners->ignore_nice_load) /* nothing to do */
+	if (input == cs_tuners->ignore_nice_load) /* nothing to do */
 		return count;
 
-	chill_tuners->ignore_nice_load = input;
+	cs_tuners->ignore_nice_load = input;
 
 	/* we need to re-evaluate prev_cpu_idle */
 	for_each_online_cpu(j) {
@@ -473,7 +504,7 @@ static ssize_t store_ignore_nice_load(struct dbs_data *dbs_data,
 		dbs_info = &per_cpu(cs_cpu_dbs_info, j);
 		dbs_info->cdbs.prev_cpu_idle = get_cpu_idle_time(j,
 					&dbs_info->cdbs.prev_cpu_wall, 0);
-		if (chill_tuners->ignore_nice_load)
+		if (cs_tuners->ignore_nice_load)
 			dbs_info->cdbs.prev_cpu_nice =
 				kcpustat_cpu(j).cpustat[CPUTIME_NICE];
 	}
@@ -483,7 +514,7 @@ static ssize_t store_ignore_nice_load(struct dbs_data *dbs_data,
 static ssize_t store_freq_step(struct dbs_data *dbs_data, const char *buf,
 		size_t count)
 {
-	struct chill_dbs_tuners *chill_tuners = dbs_data->tuners;
+	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 	unsigned int input;
 	int ret;
 	ret = sscanf(buf, "%u", &input);
@@ -498,7 +529,7 @@ static ssize_t store_freq_step(struct dbs_data *dbs_data, const char *buf,
 	 * no need to test here if freq_step is zero as the user might actually
 	 * want this, they would be crazy though :)
 	 */
-	chill_tuners->freq_step = input;
+	cs_tuners->freq_step = input;
 	return count;
 }
 
@@ -508,7 +539,7 @@ static ssize_t store_freq_step(struct dbs_data *dbs_data, const char *buf,
 static ssize_t store_sleep_depth(struct dbs_data *dbs_data, const char *buf,
 		size_t count)
 {
-	struct chill_dbs_tuners *chill_tuners = dbs_data->tuners;
+	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 	unsigned int input;
 	int ret;
 	ret = sscanf(buf, "%u", &input);
@@ -519,7 +550,7 @@ static ssize_t store_sleep_depth(struct dbs_data *dbs_data, const char *buf,
 	if (input > 5)
 		input = 5;
 
-	chill_tuners->sleep_depth = input;
+	cs_tuners->sleep_depth = input;
 	return count;
 }
 
@@ -527,7 +558,7 @@ static ssize_t store_sleep_depth(struct dbs_data *dbs_data, const char *buf,
 static ssize_t store_boost_enabled(struct dbs_data *dbs_data, const char *buf,
 		size_t count)
 {
-	struct chill_dbs_tuners *chill_tuners = dbs_data->tuners;
+	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 	unsigned int input;
 	int ret;
 	ret = sscanf(buf, "%u", &input);
@@ -540,7 +571,7 @@ static ssize_t store_boost_enabled(struct dbs_data *dbs_data, const char *buf,
 	else
 		input = 0;
 
-	chill_tuners->boost_enabled = input;
+	cs_tuners->boost_enabled = input;
 	return count;
 }
 
@@ -550,7 +581,7 @@ static ssize_t store_sleep_depth(struct dbs_data *dbs_data, const char *buf,
 >>>>>>> 2be0437dd8e1... cpufreq: Add Chill cpu gov
 		size_t count)
 {
-	struct chill_dbs_tuners *chill_tuners = dbs_data->tuners;
+	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 	unsigned int input;
 	int ret;
 	ret = sscanf(buf, "%u", &input);
@@ -573,6 +604,7 @@ static ssize_t store_sleep_depth(struct dbs_data *dbs_data, const char *buf,
 	return count;
 }
 
+<<<<<<< HEAD
 show_store_one(cs, sampling_rate);
 show_store_one(cs, up_threshold);
 show_store_one(cs, down_threshold);
@@ -602,6 +634,22 @@ show_store_one(chill, sleep_depth);
 show_store_one(chill, boost_enabled);
 show_store_one(chill, boost_count);
 >>>>>>> 7d019fa8484d... cpufreq: chill: Major cleanup, move changes from governor.h to chill.h
+=======
+	cs_tuners->boost_count = input;
+	return count;
+}
+
+show_store_one(cs, sampling_rate);
+show_store_one(cs, up_threshold);
+show_store_one(cs, down_threshold);
+show_store_one(cs, down_threshold_suspended);
+show_store_one(cs, ignore_nice_load);
+show_store_one(cs, freq_step);
+declare_show_sampling_rate_min(cs);
+show_store_one(cs, sleep_depth);
+show_store_one(cs, boost_enabled);
+show_store_one(cs, boost_count);
+>>>>>>> 55a9e8a34183... cpufreq: chill: Go back to using Conservative's tunables
 
 gov_sys_pol_attr_rw(sampling_rate);
 gov_sys_pol_attr_rw(up_threshold);
@@ -636,7 +684,7 @@ static struct attribute *dbs_attributes_gov_sys[] = {
 	NULL
 };
 
-static struct attribute_group chill_attr_group_gov_sys = {
+static struct attribute_group cs_attr_group_gov_sys = {
 	.attrs = dbs_attributes_gov_sys,
 	.name = "chill",
 };
@@ -661,13 +709,14 @@ static struct attribute *dbs_attributes_gov_pol[] = {
 	NULL
 };
 
-static struct attribute_group chill_attr_group_gov_pol = {
+static struct attribute_group cs_attr_group_gov_pol = {
 	.attrs = dbs_attributes_gov_pol,
 	.name = "chill",
 };
 
 /************************** sysfs end ************************/
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 static void save_tuners(struct cpufreq_policy *policy,
 			  struct cs_dbs_tuners *tuners)
@@ -688,8 +737,11 @@ static struct cs_dbs_tuners *alloc_tuners(struct cpufreq_policy *policy)
 =======
 static int chill_init(struct dbs_data *dbs_data)
 >>>>>>> 7d019fa8484d... cpufreq: chill: Major cleanup, move changes from governor.h to chill.h
+=======
+static int cs_init(struct dbs_data *dbs_data)
+>>>>>>> 55a9e8a34183... cpufreq: chill: Go back to using Conservative's tunables
 {
-	struct chill_dbs_tuners *tuners;
+	struct cs_dbs_tuners *tuners;
 
 	tuners = kzalloc(sizeof(*tuners), GFP_KERNEL);
 	if (!tuners) {
@@ -743,38 +795,38 @@ static int cs_init(struct dbs_data *dbs_data, struct cpufreq_policy *policy)
 	return 0;
 }
 
-static void chill_exit(struct dbs_data *dbs_data)
+static void cs_exit(struct dbs_data *dbs_data)
 {
 	//nothing to do
 }
 
 define_get_cpu_dbs_routines(cs_cpu_dbs_info);
 
-static struct notifier_block chill_cpufreq_notifier_block = {
+static struct notifier_block cs_cpufreq_notifier_block = {
 	.notifier_call = dbs_cpufreq_notifier,
 };
 
-static struct cs_ops chill_ops = {
-	.notifier_block = &chill_cpufreq_notifier_block,
+static struct cs_ops cs_ops = {
+	.notifier_block = &cs_cpufreq_notifier_block,
 };
 
-static struct common_dbs_data chill_dbs_cdata = {
-	.governor = GOV_CHILL,
-	.attr_group_gov_sys = &chill_attr_group_gov_sys,
-	.attr_group_gov_pol = &chill_attr_group_gov_pol,
+static struct common_dbs_data cs_dbs_cdata = {
+	.governor = 1,
+	.attr_group_gov_sys = &cs_attr_group_gov_sys,
+	.attr_group_gov_pol = &cs_attr_group_gov_pol,
 	.get_cpu_cdbs = get_cpu_cdbs,
 	.get_cpu_dbs_info_s = get_cpu_dbs_info_s,
-	.gov_dbs_timer = chill_dbs_timer,
-	.gov_check_cpu = chill_check_cpu,
-	.gov_ops = &chill_ops,
-	.init = chill_init,
-	.exit = chill_exit,
+	.gov_dbs_timer = cs_dbs_timer,
+	.gov_check_cpu = cs_check_cpu,
+	.gov_ops = &cs_ops,
+	.init = cs_init,
+	.exit = cs_exit,
 };
 
-static int chill_cpufreq_governor_dbs(struct cpufreq_policy *policy,
+static int cs_cpufreq_governor_dbs(struct cpufreq_policy *policy,
 				   unsigned int event)
 {
-	return cpufreq_governor_dbs(policy, &chill_dbs_cdata, event);
+	return cpufreq_governor_dbs(policy, &cs_dbs_cdata, event);
 }
 
 #ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_CHILL
@@ -782,7 +834,7 @@ static
 #endif
 struct cpufreq_governor cpufreq_gov_chill = {
 	.name			= "chill",
-	.governor		= chill_cpufreq_governor_dbs,
+	.governor		= cs_cpufreq_governor_dbs,
 	.max_transition_latency	= TRANSITION_LATENCY_LIMIT,
 	.owner			= THIS_MODULE,
 };
@@ -816,3 +868,4 @@ fs_initcall(cpufreq_gov_dbs_init);
 module_init(cpufreq_gov_dbs_init);
 #endif
 module_exit(cpufreq_gov_dbs_exit);
+
