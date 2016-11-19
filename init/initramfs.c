4f4d7642b37bd9d11e5a2123a62acc32619f2ab0
flar2 <asegaert@gmail.com>
2016-11-19 14:47:18 -0500

    initramfs: add skip_override parameter

--- init/initramfs.c
+++ init/initramfs.c
@@ -631,7 +631,7 @@ static int __init populate_rootfs(void)
 {
 	char *err;
 
-	if (do_skip_initramfs) {
+	if (!skip_override && do_skip_initramfs) {
 		if (initrd_start)
 			free_initrd();
 		return default_rootfs();
