785d43395675230c301a5691d251f619fb35b285
edison_shih <edison_shih@htc.com>
2017-06-29 10:37:18 +0800

    fs:pstore:ramoops: add new driver to append bootloader log into ramoops
    Append bootloader log into ramoops for debug purpose.
    
    Bug: 62881076
    Change-Id: Ia818e5c56b5a33293bd1614746f4174a39af764d
    Signed-off-by: edison_shih <edison_shih@htc.com>

--- include/linux/htc_debug_tools.h
+++ include/linux/htc_debug_tools.h
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 /* Copyright (c) 2013, HTC Corporation. All rights reserved.
+=======
+/* Copyright (c) 2013,2017 HTC Corporation. All rights reserved.
+>>>>>>> 47b2252826d4... fs:pstore:ramoops: add new driver to append bootloader log into ramoops
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -17,10 +21,21 @@
 #if defined(CONFIG_HTC_DEBUG_BOOTLOADER_LOG)
 ssize_t bldr_log_read_once(char __user *userbuf, ssize_t klog_size);
 ssize_t bldr_last_log_read_once(char __user *userbuf, ssize_t klog_size);
-ssize_t bldr_log_read(const void *lastk_buf, ssize_t lastk_size, char __user *userbuf,
-		size_t count, loff_t *ppos);
+ssize_t bldr_log_read(const void *lastk_buf, ssize_t lastk_size,
+	char __user *userbuf, size_t count, loff_t *ppos);
+ssize_t bldr_log_total_size(void);
 int bldr_log_init(void);
 void bldr_log_release(void);
+#else
+static inline ssize_t bldr_log_read_once(char __user *userbuf,
+	ssize_t klog_size) { return 0; }
+static inline ssize_t bldr_last_log_read_once(char __user *userbuf,
+	ssize_t klog_size) { return 0; }
+static inline ssize_t bldr_log_read(const void *lastk_buf, ssize_t lastk_size,
+	char __user *userbuf, size_t count, loff_t *ppos) { return 0; }
+static inline ssize_t bldr_log_total_size(void) { return 0; }
+static inline int bldr_log_init(void) { return 0; }
+static inline void bldr_log_release(void) { return; }
 #endif
 
 #endif /* __HTC_DEBUG_TOOLS_H__ */
