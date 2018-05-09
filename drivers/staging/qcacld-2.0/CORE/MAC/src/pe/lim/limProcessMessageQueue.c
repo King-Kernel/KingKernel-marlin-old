58c81316df84954dab74bcc0f8c5e40eef4da901
Nathan Chancellor <natechancellor@gmail.com>
2018-05-08 17:17:31 -0700

    qcacld-2.0: Fix misleading indentation warning in __limHandleBeacon
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limProcessMessageQueue.c
+++ drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limProcessMessageQueue.c
@@ -382,7 +382,11 @@ __limHandleBeacon(tpAniSirGlobal pMac, tpSirMsgQ pMsg, tpPESession psessionEntry
      else
         limProcessBeaconFrame(pMac, pRxPacketInfo, psessionEntry);
 
+<<<<<<< HEAD
      return;
+=======
+    return;
+>>>>>>> 7540926408d9... qcacld-2.0: Fix misleading indentation warning in __limHandleBeacon
 }
 
 
