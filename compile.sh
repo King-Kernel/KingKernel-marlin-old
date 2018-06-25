770fd08e7bc44751a48525db348f9a4b68e83b7e
engstk <eng.stk@sapo.pt>
2018-06-25 11:42:58 +0100

    cpufreq: blu_schedutil: Introduce initial bringup
    Import from linux 4.9 android-common plus additional changes and fixes
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-make O=out ARCH=arm64 king_defconfig
+make O=out ARCH=arm64 king1_defconfig
 make -j$(nproc --all) O=out ARCH=arm64 CC='/media/insomniac12/MATE/dtc/out/8.0/bin/clang' CLANG_TRIPLE=aarch64-linux-gnu CROSS_COMPILE_ARM32='/home/insomniac12/arm-linux-gnueabi/bin/arm-linux-gnueabi-' CROSS_COMPILE='/home/insomniac12/aarch64-linux-gnu/bin/aarch64-linux-gnu-'
