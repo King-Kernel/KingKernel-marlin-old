6004f42c98837520d78c3716bfc235375a281064
bdashore3 <bdashore3@gmail.com>
2018-11-04 11:02:44 -0500

    Fix move.sh (ME ONLY)
    "Fixes move.sh file for my machine"
    
    Signed-off-by bdashore3 <bdashore3@gmail.com>

--- move.sh
+++ move.sh
@@ -1,14 +1,14 @@
-rm -r /media/insomniac12/MATE/KingKernel-AnyKernel2/dtbs/*
+rm -r /home/kingbri1/AnyKernel2/dtbs/*
 echo "removed dtbs"
-cp /home/insomniac12/KingKernel-private/out/arch/arm64/boot/dts/htc/* /media/insomniac12/MATE/KingKernel-AnyKernel2/dtbs/
+cp /home/kingbri1/KingKernel-private/out/arch/arm64/boot/dts/htc/* /home/kingbri1/AnyKernel2/dtbs/
 echo "Added new dtbs"
-sudo chown insomniac12 /media/insomniac12/MATE/KingKernel-AnyKernel2/dtbs/*
-rm -r /media/insomniac12/MATE/KingKernel-AnyKernel2/dtbs/modules.order
-sudo chown insomniac12 /media/insomniac12/MATE/KingKernel-AnyKernel2/kernel
-rm -r /media/insomniac12/MATE/KingKernel-AnyKernel2/kernel/*
+sudo chown kingbri1 /home/kingbri1/AnyKernel2/dtbs/*
+rm -r /home/kingbri1/AnyKernel2/dtbs/modules.order
+sudo chown kingbri1 /home/kingbri1/AnyKernel2/kernel
+rm -r /home/kingbri1/AnyKernel2/kernel/*
 echo "Removed old kernel file"
-sudo chown insomniac12 /media/insomniac12/MATE/KingKernel-AnyKernel2/kernel
-cp /home/insomniac12/KingKernel-private/out/arch/arm64/boot/Image.lz4 /media/insomniac12/MATE/KingKernel-AnyKernel2/kernel
+sudo chown kingbri1 /home/kingbri1/AnyKernel2/kernel
+cp /home/kingbri1/KingKernel-private/out/arch/arm64/boot/Image.lz4 /home/kingbri1/AnyKernel2/kernel
 echo "Added new kernel file"
-sudo chown insomniac12 /media/insomniac12/MATE/KingKernel-AnyKernel2/kernel/Image.lz4
+sudo chown kingbri1 /home/kingbri1/AnyKernel2/kernel/Image.lz4
 echo "Done"
