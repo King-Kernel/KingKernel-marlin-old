fbe2becc8c182272cfa3ec4ecbbaa49e4e58d270
bdashore3 <bdashore3@gmail.com>
2018-11-04 14:21:26 -0500

    Fix kernel/sched/core.c
    "This just spits out an error that isn't needed"
    
    Signed-off-by kingbri <bdashore3@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1262,8 +1262,6 @@ void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 	if (p->sched_class->set_cpus_allowed)
 		p->sched_class->set_cpus_allowed(p, new_mask);
 
-    new_mask = adjust_cpumask(p, new_mask);
-
 	cpumask_copy(&p->cpus_allowed, new_mask);
 	p->nr_cpus_allowed = cpumask_weight(new_mask);
 }
