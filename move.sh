1b3312d8d01f09878738baa87876f06460d3e3af
bdashore3 <bdashore3@gmail.com>
2018-11-19 17:45:37 -0500

    Move.sh rework

--- move.sh
+++ move.sh
@@ -1,6 +1,6 @@
 rm -r /home/kingbri1/AnyKernel2/dtbs/*
 echo "removed dtbs"
-cp /home/kingbri1/KingKernel-private/out/arch/arm64/boot/dts/htc/* /home/kingbri1/AnyKernel2/dtbs/
+cp /home/kingbri1/KingKernel-marlin/out/arch/arm64/boot/dts/htc/* /home/kingbri1/AnyKernel2/dtbs/
 echo "Added new dtbs"
 sudo chown kingbri1 /home/kingbri1/AnyKernel2/dtbs/*
 rm -r /home/kingbri1/AnyKernel2/dtbs/modules.order
@@ -8,7 +8,7 @@ sudo chown kingbri1 /home/kingbri1/AnyKernel2/kernel
 rm -r /home/kingbri1/AnyKernel2/kernel/*
 echo "Removed old kernel file"
 sudo chown kingbri1 /home/kingbri1/AnyKernel2/kernel
-cp /home/kingbri1/KingKernel-private/out/arch/arm64/boot/Image.lz4 /home/kingbri1/AnyKernel2/kernel
+cp /home/kingbri1/KingKernel-marlin/out/arch/arm64/boot/Image.lz4 /home/kingbri1/AnyKernel2/kernel
 echo "Added new kernel file"
 sudo chown kingbri1 /home/kingbri1/AnyKernel2/kernel/Image.lz4
 echo "Done"
