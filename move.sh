83cd9dab33e7329c09dd0dd77c65a4c8a9056d3c
kingbri <bdashore3@gmail.com>
2018-12-12 15:18:15 -0500

    Adjust to new system
    "Ignore this commit"

--- move.sh
+++ move.sh
@@ -1,14 +1,14 @@
-rm -r /home/kingbri1/AnyKernel2/dtbs/*
+rm -r /home/kingbri/Vbox/AnyKernel2/dtbs/*
 echo "removed dtbs"
-cp /home/kingbri1/KingKernel-marlin/out/arch/arm64/boot/dts/htc/* /home/kingbri1/AnyKernel2/dtbs/
+cp /home/kingbri/KingKernel-marlin/out/arch/arm64/boot/dts/htc/* /home/kingbri/Vbox/AnyKernel2/dtbs/
 echo "Added new dtbs"
-sudo chown kingbri1 /home/kingbri1/AnyKernel2/dtbs/*
-rm -r /home/kingbri1/AnyKernel2/dtbs/modules.order
-sudo chown kingbri1 /home/kingbri1/AnyKernel2/kernel
-rm -r /home/kingbri1/AnyKernel2/kernel/*
+sudo chown kingbri /home/kingbri/Vbox/AnyKernel2/dtbs/*
+rm -r /home/kingbri/Vbox/AnyKernel2/dtbs/modules.order
+sudo chown kingbri /home/kingbri/Vbox/AnyKernel2/kernel
+rm -r /home/kingbri/Vbox/AnyKernel2/kernel/*
 echo "Removed old kernel file"
-sudo chown kingbri1 /home/kingbri1/AnyKernel2/kernel
-cp /home/kingbri1/KingKernel-marlin/out/arch/arm64/boot/Image.lz4 /home/kingbri1/AnyKernel2/kernel
+sudo chown kingbri /home/kingbri/Vbox/AnyKernel2/kernel
+cp /home/kingbri/KingKernel-marlin/out/arch/arm64/boot/Image.lz4 /home/kingbri/Vbox/AnyKernel2/kernel
 echo "Added new kernel file"
-sudo chown kingbri1 /home/kingbri1/AnyKernel2/kernel/Image.lz4
+sudo chown kingbri /home/kingbri/Vbox/AnyKernel2/kernel/Image.lz4
 echo "Done"
