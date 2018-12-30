88f7759e708baf84a14daa5d221fd140e4655fc6
kingbri <bdashore3@gmail.com>
2018-12-30 08:54:06 -0500

    Modify compile.sh to use new clang

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 make O=out ARCH=arm64 king_defconfig
-make -j$(nproc --all) O=out ARCH=arm64 CC='/home/kingbri/toolchains/clang-8.x/bin/clang' CLANG_TRIPLE=aarch64-linux-gnu CROSS_COMPILE_ARM32='/home/kingbri/x-tools/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-' CROSS_COMPILE='/home/kingbri/x-tools/aarch64-unknown-linux-gnu/bin/aarch64-unknown-linux-gnu-'
+make -j$(nproc --all) O=out ARCH=arm64 CC='/home/kingbri/clang/bin/clang' CLANG_TRIPLE=aarch64-linux-gnu CROSS_COMPILE_ARM32='/home/kingbri/x-tools/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-' CROSS_COMPILE='/home/kingbri/x-tools/aarch64-unknown-linux-gnu/bin/aarch64-unknown-linux-gnu-'
