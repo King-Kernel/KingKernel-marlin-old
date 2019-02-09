63b3a3d2b3eeb77ec6d3ef735a65acfbddaf1679
kdrag0n <dragon@khronodragon.com>
2019-02-08 17:30:06 -0800

    Revert "cpu_input_boost: introduce display-on stune boost floor"
    This reverts commit 06b61cc141c63840eda848fd7f0fd1c1b925625e.
    
    It turns out that because we boost on interaction and frame update, this
    is not necessary, especially with schedtune.prefer_idle = 1 for top-app
    and foreground.
    
    EDIT by kingbri: Remove the logic to check if the screen is on, just add a
    pass parameter if it happens because I don't want anything to happen
    when the screen awakens
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -35,12 +35,10 @@ module_param(input_boost_duration, short, 0644);
 static __read_mostly int input_stune_boost = CONFIG_INPUT_BOOST_STUNE_LEVEL;
 static __read_mostly int max_stune_boost = CONFIG_MAX_BOOST_STUNE_LEVEL;
 static __read_mostly int general_stune_boost = CONFIG_GENERAL_BOOST_STUNE_LEVEL;
-static __read_mostly int display_stune_boost = CONFIG_DISPLAY_BOOST_STUNE_LEVEL;
 
 module_param_named(dynamic_stune_boost, input_stune_boost, int, 0644);
 module_param(max_stune_boost, int, 0644);
 module_param(general_stune_boost, int, 0644);
-module_param(display_stune_boost, int, 0644);
 #endif
 
 /* Available bits for boost_drv state */
@@ -51,7 +49,6 @@ module_param(display_stune_boost, int, 0644);
 #define INPUT_STUNE_BOOST	BIT(4)
 #define MAX_STUNE_BOOST		BIT(5)
 #define GENERAL_STUNE_BOOST	BIT(6)
-#define DISPLAY_STUNE_BOOST	BIT(7)
 
 struct boost_drv {
 	struct kthread_worker worker;
@@ -72,7 +69,6 @@ struct boost_drv {
 	int input_stune_slot;
 	int max_stune_slot;
 	int general_stune_slot;
-	int display_stune_slot;
 };
 
 static struct boost_drv *boost_drv_g __read_mostly;
@@ -354,7 +350,6 @@ static int fb_notifier_cb(struct notifier_block *nb,
 	struct boost_drv *b = container_of(nb, typeof(*b), fb_notif);
 	struct fb_event *evdata = data;
 	int *blank = evdata->data;
-	u32 state = get_boost_state(b);
 
 	/* Parse framebuffer blank events as soon as they occur */
 	if (action != FB_EARLY_EVENT_BLANK)
@@ -362,16 +357,9 @@ static int fb_notifier_cb(struct notifier_block *nb,
 
 	/* Boost when the screen turns on and unboost when it turns off */
 	if (*blank == FB_BLANK_UNBLANK) {
-		set_boost_bit(b, SCREEN_AWAKE);
-		update_stune_boost(b, state, DISPLAY_STUNE_BOOST,
-				   display_stune_boost, &b->display_stune_slot);
-#ifdef CONFIG_CPU_INPUT_BOOST_DEBUG
-		pr_info("kicked max wake boost due to unblank event\n");
-#endif
+		;
 	} else {
 		clear_boost_bit(b, SCREEN_AWAKE);
-		clear_stune_boost(b, state, DISPLAY_STUNE_BOOST,
-				  b->display_stune_slot);
 		unboost_all_cpus(b);
 #ifdef CONFIG_CPU_INPUT_BOOST_DEBUG
 		pr_info("cleared all boosts due to blank event\n");
