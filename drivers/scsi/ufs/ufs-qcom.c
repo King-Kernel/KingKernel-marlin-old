c0a865a46ca10c0edcd70fc944afc97562a9bea0
Artem Labazov <123321artyom@gmail.com>
2019-02-15 16:52:51 +0300

    scsi: ufs: Add compile-time config to limit number of TX lines used
    Kanged from Sony copyleft archive.
    
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>
    [kdrag0n: From my testing this does not negatively impact real-world
    write speed on Micron UFS. Before: 231.52 MB/s seq and 21.61 MB/s
    random, After: 229.88 MB/s seq and 23.65 MB/s random]
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -156,10 +156,12 @@ static int ufs_qcom_enable_lane_clks(struct ufs_qcom_host *host)
 		if (err)
 			goto disable_tx_l0;
 
-		err = ufs_qcom_host_clk_enable(dev, "tx_lane1_sync_clk",
-			host->tx_l1_sync_clk);
-		if (err)
-			goto disable_rx_l1;
+#ifndef CONFIG_SCSI_UFS_RESTRICT_TX_LANES
+		/* The tx lane1 clk could be muxed, hence keep this optional */
+		if (host->tx_l1_sync_clk)
+			ufs_qcom_host_clk_enable(dev, "tx_lane1_sync_clk",
+						 host->tx_l1_sync_clk);
+#endif
 	}
 	host->is_lane_clks_enabled = true;
 	goto out;
@@ -197,8 +199,11 @@ static int ufs_qcom_init_lane_clks(struct ufs_qcom_host *host)
 		if (err)
 			goto out;
 
-		err = ufs_qcom_host_clk_get(dev, "tx_lane1_sync_clk",
-			&host->tx_l1_sync_clk);
+#ifndef CONFIG_SCSI_UFS_RESTRICT_TX_LANES
+		/* The tx lane1 clk could be muxed, hence keep this optional */
+		ufs_qcom_host_clk_get(dev, "tx_lane1_sync_clk",
+					&host->tx_l1_sync_clk);
+#endif
 	}
 out:
 	return err;
