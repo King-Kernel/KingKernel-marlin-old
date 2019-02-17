99f26364515e8f44f6e73c660470885739aa49cb
bdashore3 <bdashore3@gmail.com>
2019-02-17 16:20:59 -0500

    Edit defconfig and add perf profiles

--- AnyKernel/overlay/init.king_unified.sh
+++ AnyKernel/overlay/init.king_unified.sh
@@ -9,12 +9,45 @@ LOG_FILE=/storage/emulated/0/logs/Proflog
 prof="$1"
 
 if [ "$prof" == "battery" ]; then 
+	echo "20" > /proc/sys/vm/swappiness
+	echo "1000" > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
+	echo "1000" > /sys/devices/system/cpu/cpu1/cpufreq/schedutil/down_rate_limit_us
+	echo "1000" > /sys/devices/system/cpu/cpu2/cpufreq/schedutil/down_rate_limit_us
+	echo "1000" > /sys/devices/system/cpu/cpu3/cpufreq/schedutil/down_rate_limit_us
+	echo "100" > /sys/devices/system/cpu/cpu3/cpufreq/schedutil/hispeed_load
+	echo "10" > /sys/module/cpu_input_boost/parameters/dynamic_stune_boost
+	echo "30" > /sys/module/cpu_input_boost/parameters/input_boost_duration
+	echo "512" > /sys/block/sde/queue/read_ahead_kb
+	echo "512" > /sys/block/sdf/queue/read_ahead_kb
+	echo "307200" > /sys/module/cpu_input_boost/parameters/input_boost_freq_lp
 	echo "---------------------------------------------" | tee -a $LOG_FILE;
         echo "Battery executed" | tee -a $LOG_FILE;
 elif [ "$prof" == "balanced" ]; then 
+	echo "20" > /proc/sys/vm/swappiness
+	echo "500" > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
+	echo "500" > /sys/devices/system/cpu/cpu1/cpufreq/schedutil/down_rate_limit_us
+	echo "500" > /sys/devices/system/cpu/cpu2/cpufreq/schedutil/down_rate_limit_us
+	echo "500" > /sys/devices/system/cpu/cpu3/cpufreq/schedutil/down_rate_limit_us
+	echo "30" > /sys/devices/system/cpu/cpu3/cpufreq/schedutil/hispeed_load
+	echo "20" > /sys/module/cpu_input_boost/parameters/dynamic_stune_boost
+	echo "64" > /sys/module/cpu_input_boost/parameters/input_boost_duration
+	echo "1028" > /sys/block/sde/queue/read_ahead_kb
+	echo "1028" > /sys/block/sdf/queue/read_ahead_kb
+	echo "614000" > /sys/module/cpu_input_boost/parameters/input_boost_freq_lp
 	echo "---------------------------------------------" | tee -a $LOG_FILE;
         echo "Balanced executed" | tee -a $LOG_FILE;
 elif [ "$prof" == "performance" ]; then 
+	echo "100" > /proc/sys/vm/swappiness
+	echo "300" > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
+	echo "300" > /sys/devices/system/cpu/cpu1/cpufreq/schedutil/down_rate_limit_us
+	echo "300" > /sys/devices/system/cpu/cpu2/cpufreq/schedutil/down_rate_limit_us
+	echo "300" > /sys/devices/system/cpu/cpu3/cpufreq/schedutil/down_rate_limit_us
+	echo "15" > /sys/devices/system/cpu/cpu3/cpufreq/schedutil/hispeed_load
+	echo "50" > /sys/module/cpu_input_boost/parameters/dynamic_stune_boost
+	echo "125" > /sys/module/cpu_input_boost/parameters/input_boost_duration
+	echo "2048" > /sys/block/sde/queue/read_ahead_kb
+	echo "2048" > /sys/block/sdf/queue/read_ahead_kb
+	echo "844200" > /sys/module/cpu_input_boost/parameters/input_boost_freq_lp
 	echo "---------------------------------------------" | tee -a $LOG_FILE;
         echo "Performance executed" | tee -a $LOG_FILE;
 fi;
