2f03b16444ddfd83ee878e10d5f6e3359f856699
bdashore3 <bdashore3@gmail.com>
2019-02-18 17:46:10 -0500

    Remove blocked wakelocks
    "You can still block them if you want to, but I don't want
    blocked wakelocks anymore by default"
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT			"wlan_pno_wl;wlan_ipa;wcnss_filter_lock;[timerfd];hal_bluetooth_lock;IPA_WS;sensor_ind;wlan;netmgr_wl;qcom_rx_wakelock;wlan_wow_wl;wlan_extscan_wl;"
+#define LIST_WL_DEFAULT			""
 
 #define LENGTH_LIST_WL			255
 #define LENGTH_LIST_WL_DEFAULT		146
