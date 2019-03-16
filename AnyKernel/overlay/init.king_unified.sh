5ddbf5b17d4a1e81562217a65312695f96c7fcb0
bdashore3 <bdashore3@gmail.com>
2019-03-16 09:49:29 -0400

    AnyKernel: Modify late script
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- AnyKernel/overlay/init.king_unified.sh
+++ AnyKernel/overlay/init.king_unified.sh
@@ -17,8 +17,6 @@ if [ "$prof" == "battery" ]; then
 	echo "100" > /sys/devices/system/cpu/cpu3/cpufreq/schedutil/hispeed_load
 	echo "10" > /sys/module/cpu_input_boost/parameters/dynamic_stune_boost
 	echo "30" > /sys/module/cpu_input_boost/parameters/input_boost_duration
-	echo "512" > /sys/block/sde/queue/read_ahead_kb
-	echo "512" > /sys/block/sdf/queue/read_ahead_kb
 	echo "307200" > /sys/module/cpu_input_boost/parameters/input_boost_freq_lp
 	echo "307200" > /sys/module/cpu_input_boost/parameters/input_boost_freq_hp
 	echo "1750" > /sys/module/cpu_input_boost/parameters/frame_boost_timeout
@@ -34,8 +32,6 @@ elif [ "$prof" == "balanced" ]; then
 	echo "30" > /sys/devices/system/cpu/cpu3/cpufreq/schedutil/hispeed_load
 	echo "20" > /sys/module/cpu_input_boost/parameters/dynamic_stune_boost
 	echo "64" > /sys/module/cpu_input_boost/parameters/input_boost_duration
-	echo "1028" > /sys/block/sde/queue/read_ahead_kb
-	echo "1028" > /sys/block/sdf/queue/read_ahead_kb
 	echo "614000" > /sys/module/cpu_input_boost/parameters/input_boost_freq_lp
 	echo "404200" > /sys/module/cpu_input_boost/parameters/input_boost_freq_hp
 	echo "3250" > /sys/module/cpu_input_boost/parameters/frame_boost_timeout
@@ -51,8 +47,6 @@ elif [ "$prof" == "performance" ]; then
 	echo "15" > /sys/devices/system/cpu/cpu3/cpufreq/schedutil/hispeed_load
 	echo "50" > /sys/module/cpu_input_boost/parameters/dynamic_stune_boost
 	echo "125" > /sys/module/cpu_input_boost/parameters/input_boost_duration
-	echo "2048" > /sys/block/sde/queue/read_ahead_kb
-	echo "2048" > /sys/block/sdf/queue/read_ahead_kb
 	echo "844200" > /sys/module/cpu_input_boost/parameters/input_boost_freq_lp
 	echo "614000" > /sys/module/cpu_input_boost/parameters/input_boost_freq_hp
 	echo "15000" > /sys/module/cpu_input_boost/parameters/frame_boost_timeout
