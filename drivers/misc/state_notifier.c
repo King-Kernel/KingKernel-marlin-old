5e78def1142b0243e6c0b23ca3f4e7d77d047dfa
Danny Lin <danny@kdrag0n.dev>
2019-03-17 11:45:46 -0400

    state_notifier: Clean up display state notifier
    This makes state_notifier a completely independent driver that does not
    need edits to any other drivers in the kernel. It's also cleaner and
    more resililent to changes, with lower chances of breaking in unusual
    circumstances as this method of display state notification has been
    battle-tested in several drivers and perfected by Google and Qualcomm.
    Our previous hooks directly in the panel poweroff/poweron code may have
    triggered at the wrong times, as msm_drm_notifier events have a
    different code path that also checks for duplication and such.
    
    Additionally, a panel that is powered on is not necessarily displaying a
    picture. Powering the panel on is not the same thing as unblanking. The
    old hook made this assumption.
    
    An added benefit of the new notification source is that an always-on
    ambient display will now be considered display-off as it should be,
    which will reduce idle power consumption for AOD users.
    
    kingbri: On mdss and fb, Always on display is considered as power-off. This
    commit mainly improves the driver. My changes just converted the syntax
    from msm_drm_notifer to fb_notifier
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- drivers/misc/state_notifier.c
+++ drivers/misc/state_notifier.c
@@ -1,5 +1,5 @@
 /*
- * State notifier driver
+ * Suspend state tracker driver
  *
  * Copyright (c) 2013-2017, Pranav Vashi <neobuddy89@gmail.com>
  *           (c) 2017, Joe Maples <joe@frap129.org>
@@ -12,19 +12,54 @@
 
 #include <linux/export.h>
 #include <linux/module.h>
-#include <linux/state_notifier.h>
+#include <linux/fb.h>
 
 bool state_suspended;
+module_param(state_suspended, bool, 0444);
 
-void state_suspend(void)
+static int fb_notifier_cb(struct notifier_block *nb,
+			       unsigned long event, void *data)
 {
-	state_suspended = true;
+	struct fb_event *evdata = data;
+	unsigned int blank;
+
+	if (event != FB_EVENT_BLANK && event != FB_EARLY_EVENT_BLANK)
+		return NOTIFY_DONE;
+
+	if (!evdata || !evdata->data)
+		return NOTIFY_DONE;
+
+	blank = *(unsigned int *)evdata->data;
+
+	switch (blank) {
+	case FB_BLANK_POWERDOWN:
+		if (event == FB_EARLY_EVENT_BLANK)
+			state_suspended = true;
+		break;
+	case FB_BLANK_UNBLANK:
+		if (event == FB_EVENT_BLANK)
+			state_suspended = false;
+		break;
+	}
+
+	return NOTIFY_OK;
 }
 
-void state_resume(void)
+static struct notifier_block display_state_nb __ro_after_init = {
+	.notifier_call = fb_notifier_cb,
+};
+
+static int __init state_notifier_init(void)
 {
-	state_suspended = false;
+	int ret;
+
+	ret = fb_register_client(&display_state_nb);
+	if (ret)
+		pr_err("Failed to register fb notifier, err: %d\n", ret);
+
+	return ret;
 }
+late_initcall(state_notifier_init);
 
 MODULE_AUTHOR("Pranav Vashi <neobuddy89@gmail.com>");
 MODULE_DESCRIPTION("Suspend state tracker");
