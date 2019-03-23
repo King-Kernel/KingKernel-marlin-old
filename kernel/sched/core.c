e62d538994d4b69744ea5fff09eee8f6a860f828
bdashore3 <bdashore3@gmail.com>
2019-03-23 14:27:02 -0400

    sched/core: fix __schedule() syntax
    "3.18 doesn't have a bool. Comply to existing code formatting"
    
    Signed-off-by: Brian Dashore <bdashore3@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3291,7 +3291,7 @@ void __noreturn do_task_dead(void)
 	/* Tell freezer to ignore us: */
 	current->flags |= PF_NOFREEZE;
 
-	__schedule(false);
+	__schedule();
 	BUG();
 	/* Avoid "noreturn function does return".  */
 	for (;;)
