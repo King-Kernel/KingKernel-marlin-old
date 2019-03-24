ef6e7ebb045c2f2547817f5ece699f5500c95db3
bdashore3 <bdashore3@gmail.com>
2019-03-24 10:07:39 -0400

    devfreq_boost: update to use msm_display_state
    "Remove the screen_awake atomic and any subsequent code"
    
    Signed-off-by: Brian Dashore <bdashore3@gmail.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -6,6 +6,7 @@
 #define pr_fmt(fmt) "devfreq_boost: " fmt
 
 #include <linux/devfreq_boost.h>
+#include <linux/display_state.h>
 #include <linux/fb.h>
 #include <linux/input.h>
 
@@ -26,7 +27,6 @@ struct boost_dev {
 struct df_boost_drv {
 	struct boost_dev devices[DEVFREQ_MAX];
 	struct notifier_block fb_notif;
-	atomic_t screen_awake;
 };
 
 static struct df_boost_drv *df_boost_drv_g __read_mostly;
@@ -52,7 +52,7 @@ void devfreq_boost_kick(enum df_device device)
 	if (!d)
 		return;
 
-	if (!atomic_read(&d->screen_awake))
+ 	if (!is_display_on())
 		return;
 
 	__devfreq_boost_kick(d->devices + device);
@@ -88,7 +88,7 @@ void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
 	if (!d)
 		return;
 
-	if (!atomic_read(&d->screen_awake))
+	if (!is_display_on())
 		return;
 
 	__devfreq_boost_kick_max(d->devices + device, duration_ms);
@@ -243,16 +243,13 @@ static int fb_notifier_cb(struct notifier_block *nb,
 	struct df_boost_drv *d = container_of(nb, typeof(*d), fb_notif);
 	struct fb_event *evdata = data;
 	int *blank = evdata->data;
-	bool screen_awake;
 
 	/* Parse framebuffer blank events as soon as they occur */
 	if (action != FB_EARLY_EVENT_BLANK)
 		return NOTIFY_OK;
 
 	/* Boost when the screen turns on and unboost when it turns off */
-	screen_awake = *blank == FB_BLANK_UNBLANK;
-	atomic_set(&d->screen_awake, screen_awake);
-	if (screen_awake) {
+	if (is_display_on()) {
 		int i;
 
 		for (i = 0; i < DEVFREQ_MAX; i++)
@@ -272,7 +269,7 @@ static void devfreq_boost_input_event(struct input_handle *handle,
 	struct df_boost_drv *d = handle->handler->private;
 	int i;
 
-	if (!atomic_read(&d->screen_awake))
+	if (!is_display_on())
 		return;
 
 	for (i = 0; i < DEVFREQ_MAX; i++)
