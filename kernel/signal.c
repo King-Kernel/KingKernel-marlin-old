c021a6066fd40ecfe599dbdae744f88bf604535c
Danny Lin <danny@kdrag0n.dev>
2019-04-12 14:21:34 -0400

    signal: Rework lmkd kill acceleration code
     - Split the lmkd kill path into a separate function for readability
     - Set the task to min nice instead of promoting it to SCHED_FIFO to
     avoid starving important real-time processes
     - Allow the task to run on all CPUs instead of only the big cluster so
     it gets scheduled faster
     - Affine the task and adjust its priority after killing and disabling
     preemption to avoid scheduling in atomic context (which causes a BUG)
     - Hold a reference to the task to avoid a potential use-after-free
    
    NOTE: on 4.4 and below, oom_reaper does not exist
    
    Suggested-and-reviewed-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -1343,14 +1343,49 @@ struct sighand_struct *__lock_task_sighand(struct task_struct *tsk,
 	return sighand;
 }
 
+static int do_lmkd_kill(struct task_struct *p)
+{
+	struct task_struct *vp;
+	int ret;
+
+	/* Boost CPU and DDR bus to the max */
+	cpu_input_boost_kick_max(200);
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 200);
+
+	preempt_disable();
+	get_task_struct(p);
+	vp = find_lock_task_mm(p);
+	if (vp)
+		get_task_struct(vp);
+
+	/* Bypass the signal queue when killing */
+	ret = do_send_sig_info(SIGKILL, SEND_SIG_FORCED, p, true);
+	if (ret) {
+		if (vp) {
+			task_unlock(vp);
+			put_task_struct(vp);
+		}
+		put_task_struct(p);
+		preempt_enable();
+		return ret;
+	}
+
+	preempt_enable();
+
+	/* Increase the task's priority */
+	set_user_nice(p, MIN_NICE);
+	/* Allow the task to run on all CPUs */
+	set_cpus_allowed_ptr(p, cpu_all_mask);
+
+	put_task_struct(p);
+	return 0;
+}
+
 /*
  * send signal info to all the members of a group
  */
 int group_send_sig_info(int sig, struct siginfo *info, struct task_struct *p)
 {
-	static const struct sched_param rt_param = {
-		.sched_priority = MAX_RT_PRIO - 1
-	};
 	int ret;
 
 	rcu_read_lock();
@@ -1359,25 +1394,10 @@ int group_send_sig_info(int sig, struct siginfo *info, struct task_struct *p)
 
 	if (!ret && sig) {
 		/* Accelerate lmkd SIGKILL handling */
-		if (sig == SIGKILL && is_lmkd_pid(current->pid)) {
-			/* Boost CPU and DDR bus to the max */
-			cpu_input_boost_kick_max(200);
-			devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 200);
-
-			preempt_disable();
-
-			/* Promote the task's priority */
-			sched_setscheduler_nocheck(p, SCHED_FIFO, &rt_param);
-			/* Affine the task to high-performance CPUs */
-			set_cpus_allowed_ptr(p, cpu_perf_mask);
-
-			/* Bypass the signal queue when killing */
-			ret = do_send_sig_info(sig, SEND_SIG_FORCED, p, true);
-
-			preempt_enable();
-		} else {
+		if (sig == SIGKILL && is_lmkd_pid(current->pid))
+			ret = do_lmkd_kill(p);
+		else
 			ret = do_send_sig_info(sig, info, p, true);
-		}
 	}
 
 	return ret;
