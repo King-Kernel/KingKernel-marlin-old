f53fc141bf26456ba79ace97327ea4d375a78834
bdashore3 <bdashore3@gmail.com>
2019-04-13 12:04:56 -0400

    Remove sqlite3
    "System is no longer mountable in Q and it's impossible to install the
    sqlite3 binary without magisk modules"
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- AnyKernel/anykernel.sh
+++ AnyKernel/anykernel.sh
@@ -30,68 +30,6 @@ dump_boot;
 
 # begin ramdisk changes
 
-#functions
-keytest() {
-  ui_print "** Vol Key Test **"
-  ui_print "** Press Vol UP **"
-  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
-  return 0
-}
-
-set_bindir(){
-    bindir=/system/bin
-    xbindir=/system/xbin
-
-    # Check for existence of /system/xbin directory.
-    if [ -d /system/xbin ]; then
-        # Use /system/xbin.
-        cp $bin/sqlite3 $xbindir
-        ui_print "  • Installed sqlite to $xbindir"
-    else
-        #use /system/bin instead of /system/xbin
-        cp $bin/sqlite3 $bindir
-        ui_print "  • Installed sqlite to $bindir"
-    fi
-}
-
-
-chooseport() {
-  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
-  while true; do
-    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
-    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
-      break
-    fi
-  done
-  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
-    return 0
-  else
-    return 1
-  fi
-}
-
-chooseportold() {
-  # Calling it first time detects previous input. Calling it second time will do what we want
-  $KEYCHECK
-  SEL=$?
-  if [ "$1" == "UP" ]; then
-    UP=$SEL
-  elif [ "$1" == "DOWN" ]; then
-    DOWN=$SEL
-  elif [ $SEL -eq $UP ]; then
-    return 0
-  elif [ $SEL -eq $DOWN ]; then
-    return 1
-  else
-    ui_print "**  Vol key not detected **"
-    abort "** Use name change method in TWRP **"
-  fi
-}
-
-# Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
-KEYCHECK=$bin/keycheck
-chmod 755 $KEYCHECK
-
 #Change the custom ramdisk to the device hardware name
 mv $TMPDIR/overlay/init.king.rc $TMPDIR/overlay/init.$(getprop ro.hardware).rc
 
