48a9d266f5aa31e64d205aa10455409c1e4e2e94
bdashore3 <bdashore3@gmail.com>
2019-04-20 15:45:11 -0400

    Revert "kernel: Boost cpubw devfreq device to the max for app launches"
    This reverts commit cbee8c42c035ddd444751d2500191ac34e81497f.
    
    "Also reverts kernel: Boost all CPUs to the max when userspace launches an app"

--- kernel/fork.c
+++ kernel/fork.c
@@ -76,8 +76,6 @@
 #include <linux/aio.h>
 #include <linux/compiler.h>
 #include <linux/cpufreq.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1712,14 +1710,6 @@ long do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
-
-#ifdef CONFIG_CPU_INPUT_BOOST
-	/* Boost CPU to the max for 32 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid) && cpu_input_boost_within_input(75)) {
-		cpu_input_boost_kick_max(32);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 32);
-	}
-#endif
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
