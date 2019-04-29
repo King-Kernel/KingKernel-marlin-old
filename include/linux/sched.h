d4c318cca12478b4e6af770de6a5bc0102d439b4
bdashore3 <bdashore3@gmail.com>
2019-04-29 18:44:04 -0400

    Revert "sched/tune: Add initial support for Dynamic SchedTune Boost"
    This reverts commit 66e61da23526e739c722d5ab83f60faed198d7e8.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3475,13 +3475,6 @@ void cpufreq_add_update_util_hook(int cpu, struct update_util_data *data,
 void cpufreq_remove_update_util_hook(int cpu);
 #endif /* CONFIG_CPU_FREQ */
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-int do_stune_boost(char *st_name, int boost);
-int reset_stune_boost(char *st_name);
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
-
-extern DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
-
 #ifndef arch_scale_cpu_capacity
 static __always_inline
 unsigned long arch_scale_cpu_capacity(struct sched_domain *sd, int cpu)
@@ -3492,5 +3485,4 @@ unsigned long arch_scale_cpu_capacity(struct sched_domain *sd, int cpu)
 	return SCHED_CAPACITY_SCALE;
 }
 #endif
-
 #endif
