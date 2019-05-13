f8c53a9eb4bc60b4c674b5b8eb2c4a7fb05443d4
bdashore3 <bdashore3@gmail.com>
2019-05-13 11:09:02 -0400

    Revert "crypto: change LZ4 modules to work with new LZ4 module version"
    This reverts commit 7ba06607021edff0d39e0fb7dedd07072bd05937.

--- crypto/lz4hc.c
+++ crypto/lz4hc.c
@@ -65,15 +65,16 @@ static void lz4hc_exit(struct crypto_tfm *tfm)
 static int __lz4hc_compress_crypto(const u8 *src, unsigned int slen,
 				   u8 *dst, unsigned int *dlen, void *ctx)
 {
+	struct lz4hc_ctx *ctx = crypto_tfm_ctx(tfm);
 	size_t tmp_len = *dlen;
 	int err;
 
-	err = lz4hc_compress(src, slen, dst, &tmp_len, ctx);
+	err = lz4hc_compress(src, slen, dst, &tmp_len, ctx->lz4hc_comp_mem);
 
-	if (!out_len)
+	if (err < 0)
 		return -EINVAL;
 
-	*dlen = out_len;
+	*dlen = tmp_len;
 	return 0;
 }
 
@@ -97,13 +98,16 @@ static int lz4hc_compress_crypto(struct crypto_tfm *tfm, const u8 *src,
 static int __lz4hc_decompress_crypto(const u8 *src, unsigned int slen,
 				     u8 *dst, unsigned int *dlen, void *ctx)
 {
-	int out_len = LZ4_decompress_safe(src, dst, slen, *dlen);
+	int err;
+	size_t tmp_len = *dlen;
+	size_t __slen = slen;
 
-	if (out_len < 0)
-		return out_len;
+	err = lz4_decompress_unknownoutputsize(src, __slen, dst, &tmp_len);
+	if (err < 0)
+		return -EINVAL;
 
-	*dlen = out_len;
-	return 0;
+	*dlen = tmp_len;
+	return err;
 }
 
 static int lz4hc_sdecompress(struct crypto_scomp *tfm, const u8 *src,
