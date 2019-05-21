6a3e10830458ef206c93600b21465706a3a03fac
Danny Lin <danny@kdrag0n.dev>
2019-05-21 16:38:53 -0400

    setup: use GCC as the main compiler
    In hackbench, the kernel performs 8.17% better compiled with GCC 9 than
    Clang 9. The difference drops to 7.62% when Clang LTO is enabled, but
    the gap is still measurable.
    
    Average time across 3 runs of hackbench -p 20 -T 1000 -l 1000:
      GCC 9:            15.63 sec
      Clang 9:          17.02 sec
      Clang 9 with LTO: 16.92 sec
    
    Compiler versions and downloads:
      GCC 9: aarch64-elf-gcc (GCC) 9.1.0
        - Download: https://github.com/kdrag0n/aarch64-elf-gcc
    
      Clang 9: Android (5484270 based on r353983c) clang version 9.0.3 (https://android.googlesource.com/toolchain/clang 745b335211bb9eadfa6aa6301f84715cee4b37c5) (https://android.googlesource.com/toolchain/llvm 60cf23e54e46c807513f7a36d0a7b777920b5881) (based on LLVM 9.0.3svn)
        - Download: https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86
    
    Full benchmark data: https://docs.google.com/spreadsheets/d/1mhjyshujZz8jYI7dMoCe-yFbxymW-fWaC08vMhBbEmQ/edit
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 # Toolchain paths
 
-# Path to the root of the Clang toolchain
-tc_clang=$HOME/toolchains/clang-9.0.3
-
 # Path to the root of the 64-bit GCC toolchain
-tc_gcc=$HOME/toolchains/aarch64-linux-android-4.9
+tc=$HOME/toolchains/gcc-9.1.0
 
 # Path to the root of the 32-bit GCC toolchain
-tc_gcc32=$HOME/toolchains/arm-linux-androideabi-4.9
+tc32=$HOME/toolchains/gcc32-9.1.0
 
 # Optional: target prefix of the 64-bit GCC toolchain
 # Leave blank for autodetection
-prefix_gcc=aarch64-linux-android-
+prefix=aarch64-elf-
 
 # Optional: target prefix of the 32-bit GCC toolchain
 # Leave blank for autodetection
-prefix_gcc32=arm-linux-androideabi-
+prefix32=arm-eabi-
 
 # Number of parallel jobs to run
 # Do not remove, set to 1 for no parallelism.
@@ -27,22 +24,21 @@ jobs=$(nproc --all)
 # Load the shared helpers early to prevent duplication
 source helpers.sh
 
-gcc_bin=$tc_gcc/bin
-gcc32_bin=$tc_gcc32/bin
-clang_bin=$tc_clang/bin
-[ -z $prefix_gcc ] && prefix_gcc=$(get_gcc_prefix $gcc_bin)
-[ -z $prefix_gcc32 ] && prefix_gcc32=$(get_gcc_prefix $gcc32_bin)
+gcc_bin=$tc/bin
+gcc32_bin=$tc32/bin
+[ -z $prefix ] && prefix=$(get_gcc_prefix $gcc_bin)
+[ -z $prefix32 ] && prefix32=$(get_gcc_prefix $gcc32_bin)
 
-export LD_LIBRARY_PATH=$tc_clang/lib64:$LD_LIBRARY_PATH
-export PATH=$clang_bin:$PATH
+# Clean up traces of Clang setup script
+unset CROSS_COMPILE
+unset CROSS_COMPILE_ARM32
+unset CLANG_TRIPLE
+
+export PATH=$gcc_bin:$gcc32_bin:$PATH
 
 MAKEFLAGS+=(
-    CC=clang
-    O=out
-    CROSS_COMPILE=$gcc_bin/$prefix_gcc
-    CROSS_COMPILE_ARM32=$gcc32_bin/$prefix_gcc32
-    CLANG_TRIPLE=aarch64-linux-gnu-
+    CROSS_COMPILE=$prefix
+    CROSS_COMPILE_ARM32=$prefix32
 
-    HOSTCC=clang
-    KBUILD_COMPILER_STRING="$(get_clang_version clang)"
+    KBUILD_COMPILER_STRING="$(get_gcc_version ${prefix}gcc)"
 )
